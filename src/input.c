ae7de3bbe6964c3f2ea9279e6913dc21439e299e
Chamber6821 <vania.osinin@yandex.ru>
2022-09-20 17:47:20 +0300

    feat: parse float with dot or comma

--- src/input.c
+++ src/input.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <math.h> // for INFINITY
+#include <math.h> // for INFINITY, fabs
 #include <malloc.h>
+#include <locale.h>
 #include "input.h"
 #include "string-utils.h"
 
 void input(const char *prompt, const char *inputPattern, void *buffer);
 
-double inputDouble(const char *prompt) {
-    return inputDoubleInRange(prompt, -INFINITY, INFINITY, MAX_LIMIT | MIN_LIMIT);
-}
+bool tryInput(const char *prompt, const char *inputPattern, void *buffer);
+
+bool tryParseWithLocale(const char *source, const char *bufferPattern, void *buffer, const char *locale);
+
+bool tryParse(const char *source, const char *bufferPattern, void *buffer);
 
 double inputDoublePositive(const char *prompt) {
     return inputDoubleInRange(prompt, 0, INFINITY, MAX_LIMIT);
@@ -25,7 +28,7 @@ int inputNatural0(const char *prompt) {
 }
 
 int inputIntInRange(const char *prompt, int min, int max) {
-    while (1) {
+    while (true) {
         int value;
         input(prompt, "%d", &value);
 
@@ -35,39 +38,74 @@ int inputIntInRange(const char *prompt, int min, int max) {
     }
 }
 
+bool doubleEquals(double a, double b, double epsilon) {
+    return fabs(a - b) < epsilon;
+}
+
 double inputDoubleInRange(const char *prompt, double min, double max, RangeInclude include) {
     const char leftPar = (include & MIN_LIMIT) ? '[' : '(';
     const char rightPar = (include & MAX_LIMIT) ? ']' : ')';
 
     while (true) {
-        double value;
-        input(prompt, "%lf", &value);
+        double value = inputDouble(prompt);
 
         bool isInRange = min < value && value < max;
-        bool isMin = (include & MIN_LIMIT) && min == value;
-        bool isMax = (include & MAX_LIMIT) && max == value;
+        bool isMin = (include & MIN_LIMIT) && doubleEquals(min, value, 0.0001);
+        bool isMax = (include & MAX_LIMIT) && doubleEquals(max, value, 0.0001);
         if (isInRange || isMin || isMax) return value;
 
         printf("Must be in range %c%lf, %lf%c. Try again\n", leftPar, min, max, rightPar);
     }
 }
 
-void input(const char *prompt, const char *inputPattern, void *buffer) {
-    char *pattern = concat(inputPattern, "%n");
+double inputDouble(const char *prompt) {
+    while (true) {
+        double value = 0;
 
-    while (1) {
         printf("%s", prompt);
         char *string = lightTrimEnd(readString(stdin));
-        size_t length = strlen(string);
-
-        size_t read = 0;
-        sscanf(string, pattern, buffer, &read);
-
+        bool success = tryParseWithLocale(string, "%lf", &value, "C")
+                || tryParseWithLocale(string, "%lf", &value, "Russian");
         free(string);
 
-        if (read == length) break;
-        else printf("Wrong! Try again\n");
+        if (success) return value;
+        printf("Wrong double format! Try again\n");
     }
+}
 
+void input(const char *prompt, const char *inputPattern, void *buffer) {
+    while (!tryInput(prompt, inputPattern, buffer)) {
+        printf("Wrong! Try again\n");
+    }
+}
+
+bool tryInput(const char *prompt, const char *inputPattern, void *buffer) {
+    printf("%s", prompt);
+
+    char *string = lightTrimEnd(readString(stdin));
+    bool status = tryParse(string, inputPattern, buffer);
+    free(string);
+
+    return status;
+}
+
+bool tryParseWithLocale(const char *source, const char *bufferPattern, void *buffer, const char *locale) {
+    const char *oldLocale = setlocale(LC_NUMERIC, NULL);
+
+    setlocale(LC_NUMERIC, locale);
+    bool success = tryParse(source, bufferPattern, buffer);
+    setlocale(LC_NUMERIC, oldLocale);
+
+    return success;
+}
+
+bool tryParse(const char *source, const char *bufferPattern, void *buffer) {
+    size_t length = strlen(source);
+    size_t read = 0;
+
+    char *pattern = concat(bufferPattern, "%n");
+    sscanf(source, pattern, buffer, &read);
     free(pattern);
+
+    return read == length;
 }
